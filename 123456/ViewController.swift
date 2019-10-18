4909a5fcb059f8b0341ab7015f44e58b3589999a
Tao2564548730 <2564548730@qq.com>
2019-10-18 10:02:46 +0800

    Initial Commit

---
+++ 123456/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  123456
+//
+//  Created by W20181105881 on 2019/10/18.
+//  Copyright © 2019 W20181105881. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ 123456Tests/_23456Tests.swift
@@ -0,0 +1,34 @@
+//
+//  _23456Tests.swift
+//  123456Tests
+//
+//  Created by W20181105881 on 2019/10/18.
+//  Copyright © 2019 W20181105881. All rights reserved.
+//
+
+import XCTest
+@testable import _23456
+
+class _23456Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
